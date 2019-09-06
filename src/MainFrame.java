f06b8b52029af574c695cc18a8050b74285418a4
Ivan Rakonjac <rakonjacivan98@gmail.com>
2019-09-06 21:03:32 +0200

    Different listeners added

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class MainFrame extends JFrame {
 
@@ -16,6 +17,62 @@ public class MainFrame extends JFrame {
 
         add(textArea,BorderLayout.CENTER);
         add(button, BorderLayout.SOUTH);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                textArea.setBackground(Color.MAGENTA);
+            }
+        });
+
+        textArea.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {
+                if(keyEvent.getKeyChar()=='b'){
+                    textArea.setBackground(Color.BLACK);
+                }
+                else if(keyEvent.getKeyChar()=='o'){
+                    textArea.setBackground(Color.ORANGE);
+                }
+            }
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+
+            }
+        });
+
+        button.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                button.setText("mouse Clicked");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {
+                button.setText("mouse pressed");
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {
+                button.setText("mouse released");
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {
+                button.setText("mouse entered");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {
+                button.setText("mouse exited");
+            }
+        });
 
         setSize(600, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
