69737aed34b017ea327ac8174281299a0bb4dcfc
Ivan Rakonjac <rakonjacivan98@gmail.com>
2019-09-06 20:37:32 +0200

    Pravljenje framea

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class App {
+
+    public static void main(String[] args){
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("Hello World");
+                frame.setSize(600, 500);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+    }
+
+}
