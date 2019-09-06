e27731f0f950c062dd36394a83c6a834baa38fde
Ivan Rakonjac <rakonjacivan98@gmail.com>
2019-09-06 21:24:41 +0200

    added JScrollPanel

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 
 public class MainFrame extends JFrame {
 
-    private JTextArea textArea;
+    private TextPanel textPanel;
     private JButton button;
 
     public MainFrame(){
@@ -12,67 +12,12 @@ public class MainFrame extends JFrame {
 
         setLayout(new BorderLayout());
 
-        textArea = new JTextArea();
+        textPanel = new TextPanel();
         button = new JButton("Click me");
 
-        add(textArea,BorderLayout.CENTER);
+        add(textPanel,BorderLayout.CENTER);
         add(button, BorderLayout.SOUTH);
 
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                textArea.setBackground(Color.MAGENTA);
-            }
-        });
-
-        textArea.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent keyEvent) {
-                if(keyEvent.getKeyChar()=='b'){
-                    textArea.setBackground(Color.BLACK);
-                }
-                else if(keyEvent.getKeyChar()=='o'){
-                    textArea.setBackground(Color.ORANGE);
-                }
-            }
-
-            @Override
-            public void keyPressed(KeyEvent keyEvent) {
-
-            }
-
-            @Override
-            public void keyReleased(KeyEvent keyEvent) {
-
-            }
-        });
-
-        button.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent mouseEvent) {
-                button.setText("mouse Clicked");
-            }
-
-            @Override
-            public void mousePressed(MouseEvent mouseEvent) {
-                button.setText("mouse pressed");
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent mouseEvent) {
-                button.setText("mouse released");
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent mouseEvent) {
-                button.setText("mouse entered");
-            }
-
-            @Override
-            public void mouseExited(MouseEvent mouseEvent) {
-                button.setText("mouse exited");
-            }
-        });
 
         setSize(600, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
---
+++ src/TextPanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TextPanel extends JPanel {
+
+    private JTextArea textArea;
+
+    public TextPanel(){
+        textArea = new JTextArea();
+        setLayout(new BorderLayout());
+
+        add(new JScrollPane(textArea),BorderLayout.CENTER);
+    }
+}
